2d3519a26b2ae7d560d641aaf605cbabbce12ac7
Natalia Lypovyk <cardinnata@gmail.com>
2020-06-28 00:29:10 +0300

    Updates

---
+++ precache-manifest.d3cf0d89654c53408ea684a6fe45ecd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8021919526403d2304975fd61140d72",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56e62b504f54968fcffa",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5957342378b64cce0c76",
+    "url": "./static/js/2.bf483398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bf483398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e62b504f54968fcffa",
+    "url": "./static/js/main.1a590904.chunk.js"
+  },
+  {
+    "revision": "95cae1a700bdf76681ef",
+    "url": "./static/js/runtime-main.d33b1b1e.js"
+  }
+]);
---
+++ static/js/main.1a590904.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptender-form-libs"]=this["webpackJsonptender-form-libs"]||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var r=t(0),o=t.n(r),c=t(2),l=t.n(c),a=(t(8),function(){return o.a.createElement("div",null)});l.a.render(o.a.createElement(o.a.StrictMode,null,o.a.createElement(a,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.1a590904.chunk.js.map
